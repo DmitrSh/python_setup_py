b071515930d8db4feb05a30c84afc0970dfb7762
Dmitriy Shirchkov <dmitriy_shirchkov@mail.ru>
2019-03-24 22:55:06 +0300

    Added setup file and README

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='multi',
+      version='1.0',
+      description='Python setup.py example',
+      author='D.Shirchkov',
+      author_email='dmitriy_shirchkov@mail.ru',
+      packages=['multi']
+     )
